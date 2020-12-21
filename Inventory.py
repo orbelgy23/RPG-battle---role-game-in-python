a6fad23947feb59775095dfac1284d42b46ee4b9
orbelgy23 <76102459+orbelgy23@users.noreply.github.com>
2020-12-21 21:45:03 +0200

    Delete Inventory.py

--- Inventory.py
+++
@@ -1,8 +0,0 @@
-class Item:
-
-    def __init__(self, name, item_type, description, value, quantity=0):
-        self.name = name
-        self.item_type = item_type
-        self.description = description
-        self.value = value
-        self.quantity = quantity
